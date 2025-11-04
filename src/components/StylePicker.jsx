d62d0636fcf0b03e7401b63fe0374dda37d9e953
turbokwaka <qvikhoney@gmail.com>
2025-11-04 13:23:00 +0200

    Fixed elements structure.

--- src/components/StylePicker.jsx
+++ src/components/StylePicker.jsx
@@ -33,65 +33,53 @@ function StylePicker() {
         setAlignment(alignName);
     };
 
-    const getPresetStyle = (presetName) => {
-        if (presetName === "Difference") return "btn-accent";
-        if (presetName === "Outline") return "btn-neutral";
-        if (presetName === "Cove") return "btn-outline";
-
-        return "btn-base-100";
-    };
-
     return (
-        <div className="card w-full max-w-lg bg-base-100 p-6">
-            {}
-            <label className="label">
-                <span className="label-text">Select a preset</span>
-            </label>
-
-            {}
-            <div className="grid grid-cols-3 gap-2">
-                {presets.map((preset) => (
-                    <button
-                        key={preset}
-                        onClick={() => handlePresetClick(preset)}
-                        className={`btn h-16 
+        <fieldset className="fieldset">
+            <legend className="fieldset-legend">
+                3. Choose subtitles style
+            </legend>
+            <div className="card w-full max-w-lg bg-base-100">
+                <div className="grid grid-cols-3 gap-2">
+                    {presets.map((preset) => (
+                        <button
+                            key={preset}
+                            onClick={() => handlePresetClick(preset)}
+                            className={`btn h-16 
                             ${
                                 activePreset === preset
                                     ? "btn-primary"
-                                    : getPresetStyle(preset)
-                            }
-                        `}
-                    >
-                        {preset} {}
-                    </button>
-                ))}
-            </div>
-
-            {}
-            <label className="label mt-6">
-                <span className="label-text">Alignment</span>
-            </label>
-
-            {}
-            <div className="grid grid-cols-3 gap-2">
-                {alignments.map((align) => (
-                    <button
-                        key={align}
-                        onClick={() => handleAlignClick(align)}
-                        className={`btn h-14 
-                            ${
-                                alignment === align
-                                    ? "btn-active"
                                     : "btn-base-100"
                             }
                         `}
-                    >
-                        {}
-                        {align}
-                    </button>
-                ))}
+                        >
+                            {preset} {}
+                        </button>
+                    ))}
+                </div>
+
+                <label className="label mt-6">
+                    <span className="label-text">Alignment</span>
+                </label>
+
+                <div className="grid grid-cols-3 gap-2">
+                    {alignments.map((align) => (
+                        <button
+                            key={align}
+                            onClick={() => handleAlignClick(align)}
+                            className={`btn h-14 
+                            ${
+                                alignment === align
+                                    ? "btn-primary"
+                                    : "btn-base-100"
+                            }
+                        `}
+                        >
+                            {align}
+                        </button>
+                    ))}
+                </div>
             </div>
-        </div>
+        </fieldset>
     );
 }
 
